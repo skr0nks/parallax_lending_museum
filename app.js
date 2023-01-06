a4f8f909ef619d48539f1f53e2e5e0433cc3c9d2
skr0nks <108427178+skr0nks@users.noreply.github.com>
2023-01-06 15:10:25 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+
+document.querySelectorAll('.cursor').forEach(node=>{
+    document.addEventListener('mousemove', e=>{
+        node.style.cssText = `--move-x: ${e.clientX}px; --move-y: ${e.clientY}px;`
+    })
+})
